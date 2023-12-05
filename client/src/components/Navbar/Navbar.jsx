76daf6352fcb7f43bf11e37e3904e4cb06662ad9
Yash Gaur <yashgaur2525@gmail.com>
2023-12-05 21:15:41 +0530

    first commit

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <a className='logo' href="#">My logo</a>
+      <nav>
+
+        
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+
+      </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
